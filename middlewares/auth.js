ba0569f4a19789445bc7198ef45ae253c603fd2a
newaritra <aritraroy14.ar@gmail.com>
2022-07-28 07:55:01 +0530

    new commit

---
+++ middlewares/auth.js
@@ -0,0 +1,39 @@
+const jwt = require("jsonwebtoken");
+const runQuery = require("../adapter/mySql");
+
+async function isLoggedIn(req, res, next) {
+  try {
+    if (req.path == "/login" || req.path == "/register") {
+      return next();
+    }
+    let token = req.headers.authorization;
+    token = token.split(" ")[1];
+    if (token == "null") {
+      console.log("Toked");
+      throw "Token not valid";
+    }
+    jwt.verify(token, "SECRET", async (err, payload) => {
+      if (err) throw err;
+      else {
+        const { username, password } = payload;
+        let queryRes = await runQuery(
+          `SELECT * FROM Users WHERE username=? AND password=?`,
+          [username, password]
+        );
+        if (!queryRes || !queryRes?.length) {
+          throw "User does not exist";
+        } else if (queryRes.length == 1) {
+          next();
+        }
+      }
+    });
+  } catch (err) {
+    console.log("Error in auth middleware", err);
+    res.json({
+      error: err,
+      message: "User not logged in",
+    });
+  }
+}
+
+module.exports = { isLoggedIn };
